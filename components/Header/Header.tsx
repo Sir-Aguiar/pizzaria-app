d4defef301fa2f6aaed6a1cf5409aa177b10af93
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-04-07 17:39:04 -0400

    Header structure

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./styles/Header.module.css";
-
+import * as MD from "react-icons/md";
+import Link from "next/link";
 const Header: React.FC = () => {
-  return <header className={`${styles.header}`}>
-
-  </header>;
+  return (
+    <header className={`${styles.header}`}>
+      <span className={`${styles.logo}`}>LOGO AQUI</span>
+      <nav className={`${styles.navigation}`}>
+        <Link href="/pedir" passHref>
+          <button className={`${styles.order}`}>
+            Pedir <MD.MdDeliveryDining size={20} />
+          </button>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
