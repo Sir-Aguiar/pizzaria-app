023dbb87e88a7c0d5abead818f10d1f5c3e9684e
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-04-09 21:33:40 -0400

    Logo and stuffs

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "./styles/Header.module.css";
 import * as MD from "react-icons/md";
@@ -5,7 +6,7 @@ import Link from "next/link";
 const Header: React.FC = () => {
   return (
     <header className={`${styles.header}`}>
-      <span className={`${styles.logo}`}>LOGO AQUI</span>
+      <img src="/logo.png" alt="Logo da pizzaria" className={`${styles.logo}`} />
       <nav className={`${styles.navigation}`}>
         <Link href="/pedir" passHref>
           <button className={`${styles.order}`}>
