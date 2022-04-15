dd28105ba7d583536e23207712f55d11aa9a8b1f
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-04-15 11:42:54 -0400

    Order Header

--- pages/order/pedir.tsx
+++ pages/order/pedir.tsx
@@ -1,9 +1,26 @@
 import { NextPage } from "next";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/Pedir.module.css";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Pedir: NextPage = () => {
-  return <main className={`${styles.mainOrderContainer}`}></main>;
+  const [orderStep, setOrderStep] = useState(0);
+  const [orderStatus, setOrderStatus] = useState("");
+  useEffect(() => {
+    const orderProgress = document.querySelectorAll(`span.${styles.orderProgress}`)
+    // Algorítmo de estilização
+  }, [orderStep]);
+  return (
+    <main className={`${styles.mainOrderContainer}`}>
+      <header className={styles.orderHeader}>
+        <span className={`${styles.orderProgress}`}>Escolha</span>
+        <IoIosArrowForward className={styles.orderPr} />
+        <span className={`${styles.orderProgress}`}>Informações</span>
+        <IoIosArrowForward className={styles.orderPr} />
+        <span className={`${styles.orderProgress}`}>Finalizar</span>
+      </header>
+    </main>
+  );
 };
 
 export default Pedir;
