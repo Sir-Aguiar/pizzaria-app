a6c3181c0aff86bbf362af650d5d0db4d6996c81
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-04-15 11:05:42 -0400

    Landing zone responsivity

--- pages/order/pedir.tsx
+++ pages/order/pedir.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from "next";
 import React from "react";
+import styles from "../../styles/Pedir.module.css";
 
 const Pedir: NextPage = () => {
-  return <main></main>;
+  return <main className={`${styles.mainOrderContainer}`}></main>;
 };
 
 export default Pedir;
