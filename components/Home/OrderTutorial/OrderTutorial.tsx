708daee37454398ad6866507cc7d5e3137c28a55
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-05-10 18:44:29 -0400

    Applying slider

--- components/Home/OrderTutorial/OrderTutorial.tsx
+++ components/Home/OrderTutorial/OrderTutorial.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Slider from "../../Slider/Slider";
 import styles from "./OrderTutorial.module.css";
-
+import { Cards } from "../../../tutorial.json";
 const OrderTutorial: React.FC<OrderTutorialProps> = () => {
-  return <div className={styles.tutorialComponent}></div>;
+  return (
+    <div className={styles.tutorialComponent}>
+      <Slider Card={Cards} />
+    </div>
+  );
 };
 
 export default OrderTutorial;
