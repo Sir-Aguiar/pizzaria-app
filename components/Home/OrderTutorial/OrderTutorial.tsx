7bfb54b9787518f3e851624a586c682b7a2762ba
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-05-14 13:36:33 -0400

    Solving json import

--- components/Home/OrderTutorial/OrderTutorial.tsx
+++ components/Home/OrderTutorial/OrderTutorial.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Slider from "../../Slider/Slider";
 import styles from "./OrderTutorial.module.css";
-import { Cards } from "../../../tutorial.json";
+import * as Cards from "../../../tutorial.json";
 const OrderTutorial: React.FC<OrderTutorialProps> = () => {
   return (
     <div className={styles.tutorialComponent}>
       <h1 className={styles.mainTitle}>Como usar nossos serviços</h1>
-      <Slider Card={Cards} />
+      <Slider Card={Cards.Cards} />
     </div>
   );
 };
