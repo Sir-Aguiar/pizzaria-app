e77fc67a5110707604fb3acfeae565203fa0b8fd
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-05-21 13:03:26 -0400

    Setting cart child and food add to cart

--- components/Pedir/FoodCard/picSide.tsx
+++ components/Pedir/FoodCard/picSide.tsx
@@ -10,33 +10,33 @@ const PicSide = styled.div<PicSideProps>`
   background-position: right;
   flex: 1;
   overflow: hidden;
-  cursor:pointer;
+  cursor: pointer;
   @media (min-width: 500px) {
     max-width: 40%;
   }
   ::after {
-    content: "+";
+    content: "✓";
     color: #e0ffe7;
     position: absolute;
     display: flex;
-    height:100%;
+    height: 100%;
     width: 100%;
     align-items: center;
     justify-content: center;
-    top:-250%;
-    left:50%;
-    transform: translate(-50%,-50%);
+    top: -250%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
     font-size: 30px;
-    background-color: #ffffff5d;
-    transition: all .4s;
+    background-color: #22b44e98;
+    transition: all 0.4s;
   }
   :hover {
     ::after {
-    top:50%;
-    left:50%;
-    transform: translate(-50%,-50%);
-  }
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 `;
 export { PicSide };
