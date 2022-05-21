6e38c4122d708cf727ad3ff38dd06505007363d9
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-05-21 19:23:00 -0400

    Titling and stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,8 @@ module.exports = {
       backgroundImage: {
         'lgimageShowcase': "url('/combo-bigbanner.png')",
         'mdimageShowcase': "url('/combo-mdbanner.png')",
-        'menuImage': "url('/269.jpg')"
+        'menuImage': "url('/269.jpg')",
+        "tutorialCard":"url('/wave.svg')"
       },
       screens: {
         'lg': { "min": '740px', "max": "900px" },
@@ -38,7 +39,7 @@ module.exports = {
       },
       boxShadow: {
         'price': '2px 2px 3px rgb(60,60,60,.4), -2px -1px 3px rgb(60,60,60,.4)',
-        'tutorial-card': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
+        'tutorial-card': 'rgba(200,100,100, 0.3) 0px 1px 2px 0px, rgba(200,100,100, 0.15) 0px 1px 3px 1px',
         "food-card": "1.3px 2.5px 3px rgb(60,60,60,.38), -1.2px -1.4px 3px rgb(60,60,60, .38)",
         "order-cta": "5px 5px 8px rgb(156,56,56,.5), -5px 5px 8px rgb(67,56,189,.4)"
       }
