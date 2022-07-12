f98bfbbb69c6a670b11ad959404f7dec2d7c7a80
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-07-12 11:41:11 -0400

    Animating content in

--- components/Home/Orders/Card/Card.tsx
+++ components/Home/Orders/Card/Card.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from "react";
+import { IoIosMore, IoMdHome } from "react-icons/io";
+import { MdOutlineMenuBook } from "react-icons/md";
 import styles from "./Card.module.css";
 
 const Card: React.FC = () => {
   const Handler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const sections = document.querySelectorAll<HTMLDivElement>(`div.${styles.section}`);
     sections.forEach((sec) => {
-      if (sec.classList.contains(styles.active)){
-        // fadeout animation
-      }
-      if (sec === e.currentTarget) {
-        e.currentTarget.classList.toggle(styles.active);
+      const myCase = document.querySelector<HTMLDivElement>(
+        `.${sec.className.replaceAll(" ", ".")} > .${styles.case}`
+      )!;
+
+      sec.classList.toggle(styles.padron);
+
+      if (sec == e.currentTarget) {
+        sec.classList.toggle(styles.active);
+        myCase.classList.toggle(styles.caseActive);
         return;
       }
+      myCase.classList.remove(styles.caseActive);
       sec.classList.toggle(styles.inactive);
     });
   };
@@ -19,9 +26,22 @@ const Card: React.FC = () => {
     <div className={styles.orderCard}>
       <h1 className={styles.clientName}>Felipe Aguiar</h1>
       <div className={styles.sections}>
-        <div className={`${styles.menu} ${styles.section}`} onClick={Handler}></div>
-        <div className={`${styles.address} ${styles.section}`} onClick={Handler}></div>
-        <div className={`${styles.more} ${styles.section}`} onClick={Handler}></div>
+        <div className={`${styles.menu} ${styles.section} ${styles.padron}`} onClick={Handler}>
+          <div className={styles.case}>
+            <MdOutlineMenuBook />
+          </div>
+        </div>
+        <div className={`${styles.address} ${styles.section} ${styles.padron}`} onClick={Handler}>
+          <div className={styles.case}>
+            <IoMdHome />
+          </div>
+          <div className={styles.content}></div>
+        </div>
+        <div className={`${styles.more} ${styles.section} ${styles.padron}`} onClick={Handler}>
+          <div className={styles.case}>
+            <IoIosMore />
+          </div>
+        </div>
       </div>
       <div className="flex flex-col">
         <h2 className={styles.price}>
